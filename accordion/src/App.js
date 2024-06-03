5a4e1336569ab08729906a62e3dc61dff40d39a4
TamannaK24 <149901439+TamannaK24@users.noreply.github.com>
2024-06-03 01:41:50 -0500

    finished accordian code

---
+++ accordion/src/App.js
@@ -0,0 +1,13 @@
+import Accordian from './components/accordion';    
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordion component */}
+      <Accordian/> 
+    </div>
+  );
+}
+
+export default App;
